d6ccb5d5e79ad9deb932538a4c9e8b9e6623e560
EuclidesRamos <euclides.filho@ccc.ufcg.edu.br>
2019-10-17 09:19:55 -0300

    Factorys finalizadas, testes em andamento

---
+++ Lab2-frontend/scoord.js
@@ -0,0 +1,114 @@
+exports.disciplina = disciplina;
+exports.turma = turma;
+exports.professor = professor;
+exports.estudante = estudante;
+
+function disciplina(id, nome, creditos, preRequisitos) {
+	let _id = id;
+	let _nome = nome;
+
+	return {
+		id: () => _id,
+		get_nome: () => _nome,
+		creditos: creditos,
+		preRequisitos: preRequisitos,
+		set_nome: (novoNome) => _nome = nome
+	};
+};
+
+function turma(disciplina, periodo) {
+	let _disciplina = disciplina;
+	let _periodo = periodo;
+	let _professor;
+	let _estudantes = [];
+	let _status;
+
+	return {
+		get_disciplina: () => _disciplina,
+		get_periodo: () => _periodo,
+		get_professor: () => _professor,
+		get_status: () => _status,
+		set_professor: (novoProfessor) => _professor = novoProfessor,
+		matricular_estudante: function (novoEstudante) {
+			if (_status == "concluida") {
+				return "Disciplina já concluída";
+			} else {
+				_estudantes.forEach((element) => {
+					if (element.getMatricula() == novoEstudante.getMatricula()) {
+						return "Aluno ja matriculado";	
+					}
+				});
+				_estudantes.push(novoEstudante);
+				return "Aluno matriculado com sucesso!";
+			}
+		},
+		desmatricular_estudante: function (estudante) {
+			if (_estudantes.includes(estudante)) {
+				_estudantes.pop(estudante);	
+			}
+		},
+		set_status: function (novoStatus) {
+			if (novoStatus == "planejada" || novoStatus == "ativa" || novoStatus == "concluida") {
+				_status = novoStatus;
+			}
+		}
+	};
+};
+
+function professor(matricula, nome, email, cpf, url_de_foto) {
+	let _matricula = matricula;
+	let _nome = nome;
+	let _email = email;
+	let _cpf = cpf;
+	let _url_de_foto = url_de_foto;
+	let turmas = [];
+
+	return {
+		get_matricula: () => _matricula,
+		get_nome: () => _nome,
+		get_email: () => _email,
+		get_cpf: () => _cpf,
+		get_url_de_foto: () => _url_de_foto,
+		set_nome: (novoNome) => _nome = novoNome,
+		aloca_turma: (turma) => turmas.push(turma),
+		turmas: function (semestre) {
+			turmas.forEach((element, index) => {
+				let result = [];
+				if (element.get_periodo() == semestre) {
+					result.push(element);
+				}
+				return result;
+			});
+		}
+	};
+};
+
+function estudante(matricula, nome, email, cpf, url_de_foto) {
+	let _matricula = matricula;
+	let _nome = nome;
+	let _email = email;
+	let _cpf = cpf;
+	let _url_de_foto = url_de_foto;
+	let turmas = [];
+
+	return {
+		get_matricula: () => _matricula,
+		get_nome: () => _nome,
+		get_email: () => _email,
+		get_cpf: () => _cpf,
+		get_url_de_foto: () => _url_de_foto,
+		set_nome: (novoNome) => _nome = novoNome,
+		matricula: (turma) => turmas.push(turma),
+		turmas: function (semestre) {
+			turmas.forEach((element, index) => {
+				let result = [];
+				if (element.get_periodo() == semestre) {
+					result.push(element);
+				}
+				return result;
+			});
+		}
+	};
+};
+
+
--- Lab2-frontend/test.js
+++ Lab2-frontend/test.js
@@ -1,17 +1,82 @@
 let assert = require('assert');
+let disciplina = require('./scoord').disciplina;
+let turma = require('./scoord').turma;
 
-describe('métodos de Array', function() {
-  it('reverse inverte ordem dos elementos', function(){
-    let a = [1, 10, 3, 5];
-    assert.deepEqual([5, 3, 10, 1], a.reverse());
+describe('factory Disciplina', function() {
+  let d0;
+
+    before(async () => {
+        d0 = disciplina('prog1', 'Programação 1', 4, []);
+    })
+
+    it('deve criar disciplinas distintas a cada invocação', function(){
+        d0 = disciplina('prog1', 'Programação 1', 4, []);
+        d1 = disciplina('prog1', 'Programação 1', 4, []);
+        d2 = disciplina('prog1', 'Programação 1', 4, []);
+        assert.notEqual(d0, d1);
+        assert.notEqual(d0, d2);
+        assert.notEqual(d1, d2);
+    });
+
+    it('deve reter os dados de inicialização', function(){
+        assert.equal('prog1', d0.id());
+        assert.equal('Programação 1', d0.get_nome());
+        assert.equal(4, d0.creditos);
+        assert.deepEqual([], d0.pre_requisitos);
+    });
+
+    it('deve permitir atualização de nome', function(){
+        d0.set_nome('Programação de Computadores I')
+        assert.equal('prog1', d0.id());
+        assert.equal('Programação de Computadores I', d0.get_nome());
+        assert.deepEqual([], d0.pre_requisitos);
+    });
+
+    it('não deve permitir atualização de id via set_id', function(){
+        assert.throws(function () {
+            d0.set_id('outro')
+        }, TypeError);
+        assert.equal('prog1', d0.id());
+    });
+
+});
+
+
+describe('factory Turma', function () {
+  let t0;
+
+  before(async () => {
+      t0 = turma('prog1', '1');
+  })
+
+  it('deve criar turmas distintas a cada invocação', function () {
+      t1 = turma('prog1', '1');
+      t2 = turma('prog1', '1');
+      t3 = turma('prog1', '1');
+
+      assert.notEqual(t1, t2);
+      assert.notEqual(t1, t3);
+      assert.notEqual(t2, t3);
   });
-  it('map cria novo array a partir de função de mapeamento', function(){
-    let a = [1, 10, 3, 5];
-    assert.deepEqual([10, 100, 30, 50], a.map(e => 10 * e));
-    assert.deepEqual([true, false, true, true], a.map(e => e % 2 != 0));
+
+  it('deve reter os dados de inicialização', function () {
+      assert.equal('prog1', t0.get_disciplina());
+      assert.equal('1', t0.get_periodo());
   });
-  it('filter cria novo array com elementos filtrados', function(){
-    let a = [1, 10, 3, 5];
-    assert.deepEqual([1, 3, 5], a.filter(e => e % 2 != 0));
+
+  it('deve permitir atualização de professor', function () {
+    t0.set_professor('Dalton');
+    assert.equal('prog1', t0.get_disciplina());
+    assert.equal("Dalton", t0.get_professor());
+  });
+
+  it('deve permitir atualização de status', function () {
+      t0.set_status('ativa');
+      assert.equal('prog1', t0.get_disciplina());
+      assert.equal('ativa', t0.get_status());
+  });
+
+  it('deve permitir matricular estudante', function () {
+      
   });
 });
